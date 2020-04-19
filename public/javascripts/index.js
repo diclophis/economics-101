073b662bc5177ddb6f7fad6a6d410d79709d163d
Jon Bardin <diclophis@gmail.com>
2020-04-19 17:13:05 -0400

    working viewbox based scaling window

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,14 +9,18 @@ var globalScale = 1.0;
 var balance = 0.0;
 
 var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg.setAttribute("preserveAspectRatio", "xMinYMax slice");
+//svg.setAttribute("preserveAspectRatio", "xMinYMax slice");
+//svg.setAttribute("preserveAspectRatio", "xMidYMid slice");
+//svg.setAttribute("preserveAspectRatio", "xMinYMin slice");
+//svg.setAttribute("preserveAspectRatio", "xMaxYMid slice");
+//svg.setAttribute("preserveAspectRatio", "xMaxYMid slice");
+svg.setAttribute("preserveAspectRatio", "none");
 svg.setAttribute("version", "1.1");
 svg.setAttribute("aria-hidden", "true");
 
 var path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
 path.setAttribute('fill', "red");
 path.setAttribute('stroke', "blue");
-path.setAttribute('stroke-width', "1px");
 path.setAttribute('id', "arrow1");
 
 svg.appendChild(path);
@@ -33,13 +37,13 @@ document.body.appendChild(haltButton);
 var zoomOutButton = document.createElement('button');
 zoomOutButton.innerText = "-";
 zoomOutButton.addEventListener('click', function() {
-  globalScale *= 0.9;
+  globalScale *= 1.1;
 });
 document.body.appendChild(zoomOutButton);
 var zoomInButton = document.createElement('button');
 zoomInButton.innerText = "+";
 zoomInButton.addEventListener('click', function() {
-  globalScale *= 1.1;
+  globalScale *= 0.9;
 });
 document.body.appendChild(zoomInButton);
 
@@ -49,14 +53,14 @@ document.body.appendChild(debugSpan);
 var getFarX = function(points, scale) {
   var fx = 0;
   if (allPoints.length > 0) {
-    fx = allPoints[allPoints.length-1][0] * scale;
+    fx = allPoints[allPoints.length-1][0];
   }
 
   return fx;
 };
 
 var getOffX = function(points, graphSize, scale) {
-  var ox = graphSize[0] - getFarX(points, scale);
+  var ox = (graphSize[0]) - getFarX(points, scale);
   return ox;
 };
 
@@ -66,18 +70,20 @@ var pointsToPath = function(points, scale, graphSize) {
   var pointCount = 0;
 
   var lastPoint = null;
+  
+  var fx = getFarX(points, scale);
 
   for (var i=points.length-1; i>0; i-=1) {
     var point = points[i];
 
-    var cx = (point[0] * scale);
-    var cy = (point[1] * scale) + (0.5 * graphSize[1]);
+    var cx = (point[0]);
+    var cy = (point[1]) + (0.5 * graphSize[1]);
 
     if (lastPoint) {
-      var lx = (lastPoint[0] * scale);
-      var ly = (lastPoint[1] * scale) + (0.5 * graphSize[1]);
+      var lx = (lastPoint[0]);
+      var ly = (lastPoint[1]) + (0.5 * graphSize[1]);
 
-      if (lx > 0 && cx > 0) {
+      if (cx > (fx - (graphSize[0] * scale))) {
         d += `M ${lx} ${ly} `;
         d += `L ${cx} ${cy} `;
         pointCount += 1;
@@ -98,7 +104,7 @@ var addTestPoint = function(globalTime, frequency, amplitude) {
   var newPoint = [];
   newPoint[0] = globalTime;
 
-  newPoint[1] = (Math.sin(globalTime * frequency) * amplitude);
+  newPoint[1] = (Math.sin(globalTime * frequency) * amplitude) + amplitude;
 
   //newPoint[1] = balance;
   //balance += 1.0;
@@ -109,10 +115,12 @@ var addTestPoint = function(globalTime, frequency, amplitude) {
 var plotWindow = function(points, scale, graphSize) {
   path.setAttribute('d', pointsToPath(points, scale, graphSize));
   var ox = getOffX(points, graphSize, scale);
-  var rx = parseInt(ox * -1);
-  var lx = parseInt(rx - graphSize[0]); //parseInt(graphSize[0] - ox);
-  debugSpan.innerText = `${debugSpan.innerText} ${ox}`;
-  svg.setAttribute("viewBox", `${lx} 0 ${rx} 256`);
+  var fx = getFarX(points, scale);
+  var rx = parseInt(fx); //parseInt((ox * -1));
+  var lx = parseInt(rx - (graphSize[0] * scale)); //parseInt(graphSize[0] - ox);
+  debugSpan.innerText = `${debugSpan.innerText} ox:${ox} scale:${scale} fx:${fx}`;
+  svg.setAttribute("viewBox", `${lx} ${(graphSize[1]) - (graphSize[1] * scale * 0.5)} ${graphSize[0] * scale} ${graphSize[1] * scale}`);
+  path.setAttribute("stroke-width", scale);
 };
 
 var plotTest = function(globalTime) {
