e9a3c649110369fa56de4cfb35d8f64be75b6a32
Jon Bardin <diclophis@gmail.com>
2020-04-19 01:16:41 -0400

    wip

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,13 +2,17 @@
 
 import * as GameLoop from './game-loop.js';
 
+var allPoints = [];
+var lastTime = 0;
+var halted = false;
+
 var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg.setAttribute("viewBox", "0 0 256 256");
+//svg.setAttribute("viewBox", "0 0 256 256");
 svg.setAttribute("preserveAspectRatio", "xMinYMax slice");
 //svg.setAttribute("preserveAspectRatio", "slice");
 //svg.setAttribute("preserveAspectRatio", "XMaxYMax");
 svg.setAttribute("version", "1.1");
-svg.setAttribute("width", "256");
+svg.setAttribute("width", "512");
 svg.setAttribute("height", "256");
 svg.setAttribute("aria-hidden", "true");
 
@@ -22,43 +26,101 @@ svg.appendChild(path);
 
 document.body.appendChild(svg);
 
-var allPoints = [];
-var lastX = 0;
+var button = document.createElement('button');
+button.innerText = "halt";
+button.addObserver('click', function() {
+  halted = !halted;
+});
 
-var pointsToPath = function(points) {
+document.body.appendChild(button);
+
+var pointsToPath = function(points, scale, graphSize) {
   var d = ``;
 
-  var lastPoint = points[0];
+  var pointCount = 0;
 
-  points.forEach(function(point) {
-    d += `M ${lastPoint[0]} ${lastPoint[1]} `;
-		d += `L ${point[0]} ${point[1]} `;
+  var lastPoint = null;
+
+  var fx = 0;
+  
+  if (allPoints.length > 0) {
+    fx = allPoints[allPoints.length-1][0] * scale;
+  }
+
+  //points.forEach(function(point, index) {
+  for (var i=points.length-1; i>0; i-=1) {
+    var point = points[i];
+
+    var cx = point[0] * scale;
+    var cy = point[1] * scale;
+
+    if (lastPoint) {
+    //(points.length < windowSize) || (points.length > windowSize && index > (points.length - windowSize))) {
+      var lx = lastPoint[0] * scale;
+      var ly = lastPoint[1] * scale;
+
+      //if (i == allPoints.length -2) {
+      //  console.log(lx);
+      //}
+
+      if (cx > (fx - graphSize)) {
+        d += `M ${lx} ${ly} `;
+        d += `L ${cx} ${cy} `;
+        pointCount += 1;
+      }
+    }
+      
     lastPoint = point;
-	});
+  }
 
   d += `z`;
+
+  //console.log(pointCount);
 
   return d;
 }
 
-var plotTest = function() {
+var addTestPoint = function(globalTime, frequency, amplitude) {
   var newPoint = [];
-  newPoint[0] = lastX;
-  newPoint[1] = (Math.sin(lastX) * 10.0) + 128;
-  lastX += 1;
+  newPoint[0] = globalTime;
+  newPoint[1] = (Math.sin(globalTime * frequency) * amplitude) + 128;
 
   allPoints.push(newPoint);
-
-	path.setAttribute('d', pointsToPath(allPoints));
-
-  if (allPoints.length > 64) {
-    svg.setAttribute("viewBox", `${allPoints[allPoints.length-31][0]} 0 ${allPoints[allPoints.length-1][0]+256} 256`);
-  }
-//}, 100);
-
-  window.requestAnimationFrame(plotTest);
 };
 
-plotTest();
+var plotWindow = function(scale, graphSize) {
+  path.setAttribute('d', pointsToPath(allPoints, scale, graphSize));
+
+  if (allPoints.length > 0) {
+    var fx = allPoints[allPoints.length-1][0] * scale;
+    //if (allPoints.length > windowSize) {
+    //if (allPoints.length
+    //  //svg.setAttribute("viewBox", `${allPoints[allPoints.length-windowSize-1][0]} 0 ${allPoints[allPoints.length-1][0]+parseInt(svg.getAttribute('width'))} 256`);
+    if (fx > graphSize) {
+      svg.setAttribute("viewBox", `${fx - graphSize} 0 ${fx} 256`);
+    }
+  }
+};
+
+var plotTest = function(globalTime) {
+  var graphSize = 512;
+  var scale = 1.0;
+
+  var frameTime = globalTime - lastTime;
+
+  //elapsed += frameTime;
+
+  if (!halted) {
+    addTestPoint(globalTime, 0.025, 100.0);
+    plotWindow(scale, graphSize);
+  }
+
+  lastTime = globalTime;
+
+  window.requestAnimationFrame(plotTest);
+  //window.setTimeout(plotTest, 1);
+};
+
+plotTest(0);
 
 //window.setInterval(function() {
