d46250532a1ead0f7e9e584db31065ed4eb266e0
Jon Bardin <diclophis@gmail.com>
2020-04-20 00:57:27 -0400

    more interesting debug render

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,16 +8,8 @@ var halted = false;
 var globalScale = 1.0;
 var balance = 0.0;
 var graphSize = [512, 256];
-
 var graphDatum = [0, 0];
-
-var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg.setAttribute("preserveAspectRatio", "none");
-svg.setAttribute("version", "1.1");
-svg.setAttribute("aria-hidden", "true");
-
-
-var pathGroup = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+var elapsedTime = 0.0;
 
 var path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
 path.setAttribute('stroke', "blue");
@@ -25,37 +17,49 @@ path.setAttribute('stroke', "blue");
 var basePath = document.createElementNS("http://www.w3.org/2000/svg", 'path');
 basePath.setAttribute('stroke', "black");
 
+var pathGroup = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+pathGroup.setAttribute("transform", `scale(1, -1)`);
 pathGroup.appendChild(path);
 pathGroup.appendChild(basePath);
 
+var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.setAttribute("width", graphSize[0]);
+svg.setAttribute("height", graphSize[1]);
+svg.setAttribute("preserveAspectRatio", "none");
+svg.setAttribute("version", "1.1");
+svg.setAttribute("aria-hidden", "true");
 svg.appendChild(pathGroup);
-
-document.body.appendChild(svg);
 
 var haltButton = document.createElement('button');
 haltButton.innerText = "halt";
 haltButton.addEventListener('click', function() {
   halted = !halted;
 });
-document.body.appendChild(haltButton);
 
 var zoomOutButton = document.createElement('button');
 zoomOutButton.innerText = "-";
 zoomOutButton.addEventListener('click', function() {
   globalScale *= 1.1;
 });
-document.body.appendChild(zoomOutButton);
+
 var zoomInButton = document.createElement('button');
 zoomInButton.innerText = "+";
 zoomInButton.addEventListener('click', function() {
   globalScale *= 0.9;
+  if (globalScale < 1.0) {
+    globalScale = 1.0;
+  }
 });
+
+document.body.appendChild(svg);
+document.body.appendChild(haltButton);
+document.body.appendChild(zoomOutButton);
 document.body.appendChild(zoomInButton);
 
 var debugSpan = document.createElement("span");
 document.body.appendChild(debugSpan);
 
-var getFarX = function(points, scale) {
+var getFarX = function() {
   var fx = 0;
   if (allPoints.length > 0) {
     fx = allPoints[allPoints.length-1][0];
@@ -64,22 +68,22 @@ var getFarX = function(points, scale) {
   return fx;
 };
 
-var getOffX = function(points, graphSize, scale) {
-  var ox = (graphSize[0]) - getFarX(points, scale);
+var getOffX = function() {
+  var ox = (graphSize[0]) - getFarX();
   return ox;
 };
 
-var pointsToPath = function(points, scale, graphSize) {
+var pointsToPath = function() {
   var d = ``;
 
   var pointCount = 0;
 
   var lastPoint = null;
   
-  var fx = getFarX(points, scale);
+  var fx = getFarX();
 
-  for (var i=points.length-1; i>0; i-=1) {
-    var point = points[i];
+  for (var i=allPoints.length-1; i>0; i-=1) {
+    var point = allPoints[i];
 
     var cx = (point[0]);
     var cy = (point[1]); // + (0.5 * graphSize[1]);
@@ -88,19 +92,17 @@ var pointsToPath = function(points, scale, graphSize) {
       var lx = (lastPoint[0]);
       var ly = (lastPoint[1]); // + (0.5 * graphSize[1]);
 
-      if (cx > (fx - (graphSize[0] * scale))) {
+      //if (cx > (fx - (graphSize[0] * globalScale))) {
         d += `M ${lx} ${ly} `;
         d += `L ${cx} ${cy} `;
         pointCount += 1;
-      }
+      //}
     }
       
     lastPoint = point;
   }
 
   d += `z`;
-
-  //debugSpan.innerText = pointCount;
 
   return d;
 }
@@ -118,42 +120,49 @@ var addTestPoint = function(globalTime, frequency, amplitude) {
   allPoints.push(newPoint);
 };
 
-var plotWindow = function(points, scale, graphSize) {
-  path.setAttribute('d', pointsToPath(points, scale, graphSize));
+var addFpsPoint = function(globalTime, deltaTime) {
+  var newPoint = [];
+  newPoint[0] = globalTime;
+  newPoint[1] = deltaTime - ((1.0 / 60.0) * 1000.0);
+
+  allPoints.push(newPoint);
+};
+
+var plotWindow = function(globalTime) {
+  path.setAttribute('d', pointsToPath());
 
 
-  var ox = getOffX(points, graphSize, scale);
-  var fx = getFarX(points, scale);
-  var rx = parseInt(fx); //parseInt((ox * -1));
-  var lx = parseInt(rx - (graphSize[0] * scale)); //parseInt(graphSize[0] - ox);
-  var sw = graphSize[0] * scale;
+  var ox = getOffX();
+  var fx = globalTime; //getFarX();
+  var rx = parseInt(fx);
+  var lx = parseInt(rx - (graphSize[0] * globalScale));
+  var sw = graphSize[0] * globalScale;
 
   //debugSpan.innerText = `${debugSpan.innerText} 1xy ${allPoints[0]} ox:${ox} scale:${scale} fx:${fx}`;
 
   basePath.setAttribute('d', `M ${lx} 0 L ${rx} 0 z`);
 
-  //svg.setAttribute("viewBox", `${lx} ${0 - graphSize[1] * 0.5} ${graphSize[0] * scale} ${graphSize[1] * scale}`);
-  svg.setAttribute("viewBox", `${lx} ${graphDatum[1] - (graphSize[1] * scale * 0.5)} ${sw} ${(graphSize[1] * scale)}`);
+  svg.setAttribute("viewBox", `${lx} ${graphDatum[1] - (graphSize[1] * globalScale * 0.5)} ${sw} ${(graphSize[1] * globalScale)}`);
 
-  path.setAttribute("stroke-width", scale);
-  basePath.setAttribute("stroke-width", scale);
+  path.setAttribute("stroke-width", globalScale);
+  basePath.setAttribute("stroke-width", globalScale);
 };
 
 var plotTest = function(globalTime) {
-
-  svg.setAttribute("width", graphSize[0]);
-  svg.setAttribute("height", graphSize[1]);
-
-  //pathGroup.setAttribute("transform", `matrix(1 0 0 -1 0 ${graphSize[1]})`);
-  pathGroup.setAttribute("transform", `scale(1, -1)`);
-
   var frameTime = globalTime - lastTime;
 
-  if (!halted) {
-    addTestPoint(globalTime, (0.01), 100.0);
-  }
+  elapsedTime += frameTime;
 
-  plotWindow(allPoints, globalScale, graphSize);
+  if (!halted) {
+    //&& (elapsedTime > ((1/24) * 1000))) {
+    //addTestPoint(globalTime, (0.0125), 100.0);
+    //debugSpan.innerText = `${elapsedTime}`;
+    addTestPoint(globalTime, (0.0125), 100.0);
+    addFpsPoint(globalTime, elapsedTime);
+    plotWindow(globalTime);
+
+    elapsedTime = 0.0;
+  }
 
   lastTime = globalTime;
 
